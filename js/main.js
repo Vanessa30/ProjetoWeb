4cd31dda2c774c2acf7e0b2d6cc57c103d51f740
Samela <laniacbrito@gmail.com>
2019-11-05 22:10:35 -0300

    javascript

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$('.carousel').carousel()
+
+function exibe(classe){
+	$('.'+ classe).show();
+}
+
+function oculta(classe){
+	$('.'+ classe).hide();	
+}
+
+$(document).ready(function(){
+
+
+	$().click(function(){
+
+
+	});
+
+});
